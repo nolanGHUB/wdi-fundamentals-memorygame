db56e88b223523a2b248a5ccde8bd200f59c00c8
nolanG <nolanrgiles@gmail.com>
2019-11-03 18:16:15 -0500

    finished game, added reset button and score features.  Cards flip appropriately with timeouts.

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,30 +21,83 @@ var cards = [
 	}
 ];
 var cardsInPlay = [];
+var cardsInPlayId = [];
+var totalCardsInPlay = [];
+var correctPairCounter = 0;
+var score = 0;
+var resetButton = document.getElementById("resetButton");
+resetButton.addEventListener('click', resetBoard);
 
 function createBoard() {
 	for (i = 0; i < cards.length; i++) {
 		var cardElement = document.createElement('img');
 		cardElement.setAttribute('src', 'images/back.png');
 		cardElement.setAttribute('data-id', i);
+		cardElement.id = ("card" + i);
 		cardElement.addEventListener('click', flipCard);
 		document.getElementById('game-board').appendChild(cardElement);
 	}
+}
+
+function resetBoard() { //logic for game reset button so no page-refresh necessary
+	var boardList = document.getElementById("game-board");
+	var total = boardList.childNodes.length;
+	for (i = 0; i < total; i++) {
+		boardList.removeChild(boardList.childNodes[0]);
+	}
+	failArrayReset();
+	correctPairCounter = 0;
+	createBoard();
+	this.style.display = "none";
+}
+
+function failArrayReset() { //reset game logic arrays
+	cardsInPlay = [];
+	cardsInPlayId = [];
+}
+
+function failCardReset() { //flip the failed match back over.
+	for (i = 0; i < cardsInPlayId.length; i++) {
+		document.getElementById("card" + cardsInPlayId[i]).setAttribute('src', 'images/back.png');
+	}
+	failArrayReset();
+}
+
+function gameWinCheck() {
+	if ((correctPairCounter * 2) === cards.length) {
+		alert("You Won!");
+		resetButton.style.display = "inline-block";
+	}
+}
+
+function updateScore() {
+	//set the html score value to reflect the games 
+	//use .textContent
+	document.getElementById("scoreNum").textContent = score;
 }
 
 function checkForMatch() {
 	if (cardsInPlay.length === 2){
 		if (cardsInPlay[0] === cardsInPlay[1]){
 			console.log("You found a match!");
+			correctPairCounter++;
+			score += 2;
+			updateScore();
+			gameWinCheck();
+			failArrayReset();
 		}
 		else {
 			console.log("Sorry, try again.");
+			score -= 1;
+			updateScore();
+			setTimeout(failCardReset, 800);
 		}
 	}
 }
 
 function flipCard() {
 	var cardId = this.getAttribute('data-id');
+	cardsInPlayId.push(cardId);
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
@@ -54,17 +107,3 @@ function flipCard() {
 }
 
 createBoard();
-//flipCard(0);
-//flipCard(2);
-
-
-
-//var cardOne = cards[0];
-//cardsInPlay.push(cardOne);
-//var cardTwo = cards[2];
-//cardsInPlay.push(cardTwo);
-//console.log(cardsInPlay.length);
-//console.log("User flipped " + cardsInPlay[0]);
-//console.log("User flipped " + cardsInPlay[1]);
-
-//alert('Hello, friends.');
