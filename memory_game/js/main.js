c71a5e91b6fae48353dd22b0527ffb7aa486a981
nolanG <nolanrgiles@gmail.com>
2019-11-03 16:27:04 -0500

    finished basic version of game

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,6 +22,16 @@ var cards = [
 ];
 var cardsInPlay = [];
 
+function createBoard() {
+	for (i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
 function checkForMatch() {
 	if (cardsInPlay.length === 2){
 		if (cardsInPlay[0] === cardsInPlay[1]){
@@ -33,15 +43,19 @@ function checkForMatch() {
 	}
 }
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 }
-flipCard(0);
-flipCard(2);
+
+createBoard();
+//flipCard(0);
+//flipCard(2);
 
 
 
